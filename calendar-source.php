c8f874be409dd7edd6db9010d12eaba9a93b05c5
TSASStallings <jstallings@tsas.org>
2017-02-11 11:43:22 -0600

    Update calendar-source.php

--- calendar-source.php
+++ calendar-source.php
@@ -11,7 +11,7 @@ CHANGE THESE VARIABLES
 
 //This is the URL of your Calendar. It's the 'src' attribute of the iFrame tag in the embed code that Google gives you.
 //If you go to this URL, you can see the default styling of the calendar, which we're trying to change.
-$my_calendar="https://www.google.com/calendar/embed?src=envql02v2rijr5doiku8mulu3o%40group.calendar.google.com&ctz=America/New_York";
+$my_calendar="https://calendar.google.com/calendar/embed?src=tsas.org_2d39323437313838312d383531%40resource.calendar.google.com&ctz=America/Chicago";
 
 //The location of our CSS file
 $our_css_url = 'css/custom-calendar.css';
